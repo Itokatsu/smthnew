635888c894b1af665b0336196066235e6322a887
itokatsu <itokatsu@protonmail.com>
2020-04-14 15:51:15 +0200

    Updates

---
+++ precache-manifest.18d38b11a64c7c03a814fbb3d2442877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccac347e4b7aaaf5afc92d766cce3f7f",
+    "url": "/smthnew/index.html"
+  },
+  {
+    "revision": "f0046a7aab83cb1705e1",
+    "url": "/smthnew/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79db7d4981814e0cb005",
+    "url": "/smthnew/static/js/2.455f1b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smthnew/static/js/2.455f1b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0046a7aab83cb1705e1",
+    "url": "/smthnew/static/js/main.8f3d8f23.chunk.js"
+  },
+  {
+    "revision": "faaa5fb0d04033ca784a",
+    "url": "/smthnew/static/js/runtime-main.9628c1f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smthnew/static/media/logo.5d5d9eef.svg"
+  }
+]);
